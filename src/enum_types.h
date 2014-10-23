41093d3c03277bfeb20009ed96f4540d3d6a6367
Andre Pereira <lazeroth89@gmail.com>
2014-10-23 15:28:42 +0100

    making it work

--- src/enum_types.h
+++ src/enum_types.h
@@ -21,3 +21,10 @@ enum class Report {
 	Verbose=20,
 	Csv=21
 };
+
+template <typename Enumeration>
+auto as_integer(Enumeration const value)
+	-> typename std::underlying_type<Enumeration>::type
+{
+	return static_cast<typename std::underlying_type<Enumeration>::type>(value);
+}
